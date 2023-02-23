5f99825acf281068a27488bcb8cee5a658368ed5
ardijanr <a.rexhaj@stud.uis.no>
2023-02-23 10:57:54 +0100

    converting python to rust

--- hdf5_to_geotiff/src/main.rs
+++ hdf5_to_geotiff/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+use std::fmt::format;
+use std::fs;
+
+
+const DATA_DIR: &str = "";
+
+fn main(){
+
 }
+
+
+fn convert_downloaded_data()->Option<bool>{
+    let paths = fs::read_dir("./").unwrap();
+
+    return Some(true);
+}
+
+
