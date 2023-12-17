0963a10cd5bfa45aa6e5954b66e4a9669669835e
ardijan@rexhaj <a.rexhaj@stud.uis.no>
2023-12-17 18:56:05 +0100

    moved some docer files, and working on world map generation

--- map_generation/src/stencil.rs
+++ map_generation/src/stencil.rs
@@ -5,8 +5,11 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::ops::AddAssign;
+use std::path::Path;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::{mpsc, Arc, RwLock};
+use std::thread::sleep;
+use std::time::Duration;
 use std::{thread, vec};
 use tiff::decoder::{Decoder, DecodingResult};
 use tiff::encoder::colortype::{ColorType, RGB8};
@@ -19,16 +22,22 @@ use geo::prelude::*;
 use crate::common::{
     generate_gradient, get_cache_from_file, length, write_cache_to_file, ParMatrix,
 };
+const THREADS: usize = 28;
+// Size of entire world
+// const IMG_WIDTH: usize = 86400;
+// const IMG_HEIGHT: usize = 36000;
 
-const THREADS: usize = 30;
+// One tile
+// const IMG_WIDTH: usize = 86400;
+// const IMG_HEIGHT: usize = 36000;
 
 // This will change for deployment
 const TEST_IMAGE: &str =
-    "archive/VNP46A2/Gap_Filled_DNB_BRDF-Corrected_NTL/2023/335/merged_data_2023_335.tif";
+    "archive/VNP46A2/Gap_Filled_DNB_BRDF-Corrected_NTL/2023/336/merged_data_2023_336.tif";
 const PIXEL_DIM: f64 = 0.004_166_666_666_666_667;
 const MAX_DIST: usize = 3000;
 
-pub fn stencil() -> Arc<ParMatrix> {
+pub fn stencil(img_width: usize, img_height: usize) -> Arc<ParMatrix> {
     //Load the tile
     let input_file = File::open(TEST_IMAGE).expect("Failed to open file");
 
@@ -62,10 +71,7 @@ pub fn stencil() -> Arc<ParMatrix> {
     // This type is being wrapped in two other types at the same time
     // Par matrix ensures interior mutability is possible
     // While ARC allows sharing of a refrence between threads.
-    let result_image = Arc::new(ParMatrix::new(36000, 86400));
-
-    let offset_lat: f64 = -10.;
-    let offset_lon: f64 = 50.;
+    let result_image = Arc::new(ParMatrix::new(img_height, img_width));
 
     let dim = dim as i32;
 
@@ -107,25 +113,27 @@ pub fn stencil() -> Arc<ParMatrix> {
             }
 
             //Calculate projection distortion in x
-            let p1 = point!(
-                x: offset_lat + (s_x as f64 * PIXEL_DIM),
-                y: offset_lon + (s_y as f64 * PIXEL_DIM)
-            );
-            let p2 = point!(
-                x: offset_lat + ((s_x + 1) as f64 * PIXEL_DIM),
-                y: offset_lon + ((s_y) as f64 * PIXEL_DIM)
-            );
-            let delta_x = (p1.geodesic_distance(&p2) / 100.) as f32;
+            // let p1 = point!(
+            //     x: offset_lat + (s_x as f64 * PIXEL_DIM),
+            //     y: offset_lon + (s_y as f64 * PIXEL_DIM)
+            // );
+            // let p2 = point!(
+            //     x: offset_lat + ((s_x + 1) as f64 * PIXEL_DIM),
+            //     y: offset_lon + ((s_y) as f64 * PIXEL_DIM)
+            // );
+            // let delta_x = (p1.geodesic_distance(&p2) / 100.) as f32;
 
-            let p2 = point!(
-                x: offset_lat + ((s_x) as f64 * PIXEL_DIM),
-                y: offset_lon + ((s_y + 1) as f64 * PIXEL_DIM)
-            );
-            let delta_y = (p1.geodesic_distance(&p2) / 100.) as f32;
+            // let p2 = point!(
+            //     x: offset_lat + ((s_x) as f64 * PIXEL_DIM),
+            //     y: offset_lon + ((s_y + 1) as f64 * PIXEL_DIM)
+            // );
+            // let delta_y = (p1.geodesic_distance(&p2) / 100.) as f32;
 
             // This calculates how wide and tall the stencil is
-            let stencil_size_x = (garstang_cache_ref[c as usize][3000] as f32 / delta_x) as i32;
-            let stencil_size_y = (garstang_cache_ref[c as usize][3000] as f32 / delta_y) as i32;
+            // let stencil_size_x = (garstang_cache_ref[c as usize][3000] as f32 / delta_x) as i32;
+            // let stencil_size_y = (garstang_cache_ref[c as usize][3000] as f32 / delta_y) as i32;
+            let stencil_size_x = garstang_cache_ref[c as usize][3000] as i32;
+            let stencil_size_y = stencil_size_x;
 
             let s_x = s_x as i32;
             let s_y = s_y as i32;
@@ -134,8 +142,10 @@ pub fn stencil() -> Arc<ParMatrix> {
             for y_stencil_index in -stencil_size_y..stencil_size_y {
                 for x_stencil_index in -stencil_size_x..stencil_size_x {
                     let dist = length(
-                        delta_x * x_stencil_index as f32,
-                        delta_y * y_stencil_index as f32,
+                        // delta_x * x_stencil_index as f32,
+                        // delta_y * y_stencil_index as f32,
+                        x_stencil_index as f32,
+                        y_stencil_index as f32,
                     );
 
                     //If the distance is greater than 299.9km
@@ -159,7 +169,7 @@ pub fn stencil() -> Arc<ParMatrix> {
                     result_image_ref.write(
                         x as usize,
                         y as usize,
-                        garstang_cache_ref[c as usize][dist] as u32,
+                        garstang_cache_ref[c as usize][dist],
                     );
                 }
             }
@@ -177,26 +187,21 @@ pub fn stencil() -> Arc<ParMatrix> {
     result_image
 }
 
-// fn tiff(results: Vec<Vec<u8>>) {
-//     let width = 86400;
-//     let height = 36000;
-//     let file = File::create("large_image.tiff").unwrap();
-//     let mut w = BufWriter::new(file);
+fn get_image_dimensions() -> (usize, usize) {
+    let file_path = Path::new(TEST_IMAGE);
+    let file = File::open(file_path).expect("Cannot open file");
 
-//     let mut encoder = encoder::TiffEncoder::new_big(w).unwrap();
-//     let mut image = encoder.new_image::<RGB8>(width, height).unwrap();
-//     image.rows_per_strip(1).unwrap();
+    let mut decoder = Decoder::new(file).expect("Cannot create TIFF decoder");
+    let dimensions = decoder.dimensions().expect("Cannot read dimensions");
 
-//     for i in 0..results.len() {
-//         image.write_data();
-//         // image.write_strip(&results[i]).unwrap();
-//     }
-//     image.finish().unwrap();
-// }
-
+    (dimensions.0 as usize, dimensions.1 as usize)
+}
 pub fn generate_image() {
+    let (img_width, img_height) = get_image_dimensions();
+
+    let result = stencil(img_width, img_height);
+
     let gradient = Arc::new(generate_gradient());
-    let result = stencil();
 
     let (tx, rx) = mpsc::channel::<(usize, Vec<u8>)>();
     let current_working_index: Arc<RwLock<usize>> = Arc::new(RwLock::new(0));
@@ -205,13 +210,13 @@ pub fn generate_image() {
     // receives finished results however it must wait for the correct ordered
     // result to finish before it can write it
     let writer_thread = thread::spawn(move || {
-        let width = 86400;
-        let height = 36000;
-        let file = File::create("large_image.tiff").unwrap();
+        let file = File::create("map_generation/tests/large_image.tiff").unwrap();
         let w = BufWriter::new(file);
 
         let mut encoder = encoder::TiffEncoder::new_big(w).unwrap();
-        let mut image = encoder.new_image::<RGB8>(width, height).unwrap();
+        let mut image = encoder
+            .new_image::<RGB8>(img_width as u32, img_height as u32)
+            .unwrap();
         image
             .encoder()
             .write_tag(Tag::Artist, "Image-tiff")
@@ -235,7 +240,7 @@ pub fn generate_image() {
                 needed_row += 1;
             }
         }
-
+        println!("I wrote {} lines", needed_row);
         image.finish().unwrap();
     });
 
@@ -250,7 +255,7 @@ pub fn generate_image() {
             let index: usize;
             if let Ok(mut i) = cwi.write() {
                 index = *i;
-                if index >= 36000 {
+                if index >= img_height {
                     break;
                 }
                 i.add_assign(1);
@@ -267,89 +272,12 @@ pub fn generate_image() {
     for j in reader_threads {
         j.join().unwrap();
     }
+    // Give reader thread a chance to drain the channel before we drop it.
+    sleep(Duration::from_secs(2));
+    // Drop the sending channel such that the reader knows nothing else will be recived.
+    drop(tx);
 
     writer_thread.join().unwrap();
-
-    // for i in 0..height {
-    //     // println!("{}", &i);
-
-    //     let strip = convert_results_to_rgb(result.read_row(i as usize), &gradient);
-    //     println!("Strip length is : {}", strip.len());
-    //     println!("Strip length is : {:?}", result.dimensions());
-    //     image.write_strip(&strip).unwrap();
-    // }
-    // image.finish().unwrap();
-
-    // Original code below
-
-    // for y in 0..86400 as usize {
-    //     for x in 0..36000 as usize {
-    //         let scaled = (result.read(x, y) as f64).sqrt() / 355.;
-
-    //         let color = gradient.at(scaled).to_rgba8();
-    //         let mut alpha: u32 = (scaled * 5000.) as u32;
-    //         if alpha > 255 {
-    //             alpha = 254;
-    //         }
-    //         generated_image.get_pixel_mut(x as u32, y as u32).0 =
-    //             [color[0], color[1], color[2], alpha as u8];
-    //     }
-    // }
-
-    // generated_image
-    //     .save(format!("./map_generation/tests/uk_test1.tif"))
-    //     .unwrap();
-}
-
-pub fn generate_image_multithreaded_read() {
-    let width = 86400;
-    let height = 36000;
-    let file = File::create("large_image.tiff").unwrap();
-    let w = BufWriter::new(file);
-
-    let mut encoder = encoder::TiffEncoder::new_big(w).unwrap();
-    let mut image = encoder.new_image::<RGB8>(width, height).unwrap();
-    image
-        .encoder()
-        .write_tag(Tag::Artist, "Image-tiff")
-        .unwrap();
-
-    image.rows_per_strip(1).unwrap();
-
-    let gradient = generate_gradient();
-    let result = stencil();
-
-    //n threads will in paralell read off the data calling convert results to rgb
-
-    for i in 0..height {
-        // println!("{}", &i);
-
-        let strip = convert_results_to_rgb(result.read_row(i as usize), &gradient);
-        println!("Strip length is : {}", strip.len());
-        println!("Strip length is : {:?}", result.dimensions());
-        image.write_strip(&strip).unwrap();
-    }
-    image.finish().unwrap();
-
-    // Original code below
-
-    // for y in 0..86400 as usize {
-    //     for x in 0..36000 as usize {
-    //         let scaled = (result.read(x, y) as f64).sqrt() / 355.;
-
-    //         let color = gradient.at(scaled).to_rgba8();
-    //         let mut alpha: u32 = (scaled * 5000.) as u32;
-    //         if alpha > 255 {
-    //             alpha = 254;
-    //         }
-    //         generated_image.get_pixel_mut(x as u32, y as u32).0 =
-    //             [color[0], color[1], color[2], alpha as u8];
-    //     }
-    // }
-
-    // generated_image
-    //     .save(format!("./map_generation/tests/uk_test1.tif"))
-    //     .unwrap();
 }
 
 // The input and output look the same, but the output is 3x longer
@@ -427,14 +355,14 @@ fn get_or_create_garstang_cache() -> Vec<Vec<u32>> {
 // This should be done in paralell
 // pub fn generate_image() {
 //     create_and_save_empty_image()
-//     let mut generated_image: RgbaImage = ImageBuffer::new(86400, 36000);
+//     let mut generated_image: RgbaImage = ImageBuffer::new(IMG_WIDTH, IMG_HEIGHT);
 
 //     let gradient = generate_gradient();
 
 //     let result = stencil();
 
-//     for y in 0..86400 as usize {
-//         for x in 0..36000 as usize {
+//     for y in 0..IMG_WIDTH as usize {
+//         for x in 0..IMG_HEIGHT as usize {
 //             let scaled = (result.read(x, y) as f64).sqrt() / 355.;
 
 //             let color = gradient.at(scaled).to_rgba8();
